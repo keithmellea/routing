5e429f8c26a2d7864eb6267cae82bf285655c753
kbetker <kevin@betker.org>
2021-03-23 15:40:42 -0500

    on to phase 3 maybe

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const app = express();
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+    // res.render("index", {method: req.method, path: req.path, randomNumber: Math.floor(Math.random())})
+    // console.log(req.method, req.path, Math.floor(Math.random()));
+    });
+
+app.all("/:id", (req, res) => {
 // res.send("Hello from Express!");
-res.render("index", {method: req.method, path: req.path, randomNumber: Math.floor(Math.random())})
+res.render("index", {method: req.method, path: req.path, randomNumber: Math.floor(Math.random() * 10)})
 console.log(req.method, req.path, Math.floor(Math.random()));
 });
 
