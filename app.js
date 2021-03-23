2f84cabf41b68a2f99cc4980021bad83bb83f9b4
Keith Mellea <keithmellea@gmail.com>
2021-03-23 16:13:49 -0400

    working on phase 2

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express();
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-res.send("Hello from Express!");
-
+// res.send("Hello from Express!");
+res.render("index", {method: req.method, path: req.path, randomNumber: Math.floor(Math.random())})
+console.log(req.method, req.path, Math.floor(Math.random()));
 });
 
-
+// app.get("/", (req, res) => {
+//     // res.send
+// })
 
 app.listen(8081, () => console.log(`listening on port 8081`))
